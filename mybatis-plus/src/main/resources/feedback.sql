c65a759b51d401e99675d785417ce089e0d8fda1
Buck Titus <37321423+fatcatmaster@users.noreply.github.com>
2023-10-17 17:49:19 +0800

    Update feedback.sql

--- mybatis-plus/src/main/resources/feedback.sql
+++ mybatis-plus/src/main/resources/feedback.sql
@@ -18,12 +18,12 @@ CREATE TABLE `key_value` (
 -- Records of key_value
 -- ----------------------------
 BEGIN;
-INSERT INTO `key_value` (`id`, `name`, `value`, `create_time`, `update_time`) VALUES (1, 'tom', '9EF7IWjDrYMD4OLH22vmNj8SOVpOToZ7', '2023-10-17 11:38:10', '2023-10-17 11:39:22');
-INSERT INTO `key_value` (`id`, `name`, `value`, `create_time`, `update_time`) VALUES (2, 'jerry', 'QP5KB194VMC9nVtVRBrWFiip3glKYI7w', '2023-10-17 11:38:24', '2023-10-17 11:39:28');
-INSERT INTO `key_value` (`id`, `name`, `value`, `create_time`, `update_time`) VALUES (3, 'john', 'oVdiGScCD2O00KdAwo9Qs7vvpeHypli3', '2023-10-17 11:38:31', '2023-10-17 11:39:50');
-INSERT INTO `key_value` (`id`, `name`, `value`, `create_time`, `update_time`) VALUES (4, 'sun', 'IsH1fY7QF9Vc3hj8VtUjP1IDHEO1E3JC', '2023-10-17 11:38:38', '2023-10-17 11:40:00');
-INSERT INTO `key_value` (`id`, `name`, `value`, `create_time`, `update_time`) VALUES (5, 'maia', 'SKJTwRgJKn0JJVAHdXjd9sBLPPRrHptj', '2023-10-17 11:38:43', '2023-10-17 11:40:52');
-INSERT INTO `key_value` (`id`, `name`, `value`, `create_time`, `update_time`) VALUES (6, 'fern', 'HaZSZnlKFEPbDc8FCxe4G3pbI31uOyfc', '2023-10-17 11:38:47', '2023-10-17 11:40:55');
+INSERT INTO `key_value` (`id`, `name`, `value`, `create_time`, `update_time`) VALUES (1, 'tom', '9EF7IWjDrYMD4OLH22vmNj8SOVpOToZ7', NOW(), NOW());
+INSERT INTO `key_value` (`id`, `name`, `value`, `create_time`, `update_time`) VALUES (2, 'jerry', 'QP5KB194VMC9nVtVRBrWFiip3glKYI7w', NOW(), NOW());
+INSERT INTO `key_value` (`id`, `name`, `value`, `create_time`, `update_time`) VALUES (3, 'john', 'oVdiGScCD2O00KdAwo9Qs7vvpeHypli3', NOW(), NOW());
+INSERT INTO `key_value` (`id`, `name`, `value`, `create_time`, `update_time`) VALUES (4, 'sun', 'IsH1fY7QF9Vc3hj8VtUjP1IDHEO1E3JC', NOW(), NOW());
+INSERT INTO `key_value` (`id`, `name`, `value`, `create_time`, `update_time`) VALUES (5, 'maia', 'SKJTwRgJKn0JJVAHdXjd9sBLPPRrHptj', NOW(), NOW());
+INSERT INTO `key_value` (`id`, `name`, `value`, `create_time`, `update_time`) VALUES (6, 'fern', 'HaZSZnlKFEPbDc8FCxe4G3pbI31uOyfc', NOW(), NOW());
 COMMIT;
 
 SET FOREIGN_KEY_CHECKS = 1;
